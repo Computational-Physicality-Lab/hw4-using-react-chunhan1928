843b60718ea591830010cb1362b47e0aece971f8
b07902059 <45457598+b07902059@users.noreply.github.com>
2023-05-02 16:59:45 +0800

    feat:add Product Page Quantity, Size and AddToCart Button. Also adjust some design.

--- scotty-shirts/src/routes/product.jsx
+++ scotty-shirts/src/routes/product.jsx
@@ -4,12 +4,18 @@ import { useParams } from "react-router-dom"
 import { useState } from "react";
 import './product.css'
 
+const sizeList = ['Size', 'Women’s XS', 'Women’s S', 'Women’s M', 'Women’s L', 'Women’s XL', 'Women’s 2XL',
+    'Men’s XS', 'Men’s S', 'Men’s M', 'Men’s L', 'Men’s XL', 'Men’s 2XL']
+
 export default function Product() {
     const [side, setSide] = useState('front')
     const [color, setColor] = useState('white')
+    const [size, setSize] = useState('Size:')
+    const [quantity, setQuantity] = useState(1)
     const { id } = useParams();
     const shirt = shirts.filter(shirt => shirt.name === id)[0] || no_shirt
     const image = shirt['colors'][color][side] || shirt['default'][side] || shirt['default']['front'] || ''
+    const addToCartBtnDisabled = Boolean(!shirt.price || size === 'Size:')
 
     return (
         <div id="product-content">
@@ -24,12 +30,12 @@ export default function Product() {
                         {shirt.description || "No Description"}
                     </p>
                     <nav id="shirt-side-nav">
-                        <span>Side:</span>
+                        <label>Side: </label>
                         <button onClick={() => { setSide('front') }}>Front</button>
                         <button onClick={() => { setSide('back') }}>Back</button>
                     </nav>
                     <nav id="shirt-color-nav">
-                        <span>Color:</span>
+                        <label>Color: </label>
                         {Object.keys(shirt.colors).map(color =>
                             <button
                                 onClick={() => { setColor(color) }}
@@ -40,8 +46,48 @@ export default function Product() {
                             </button>
                         )}
                     </nav>
+                    <nav id="shirt-quantity-nav">
+                        <label>Quantity: </label>
+                        <select
+                            id="shirt-quantity-nav-select"
+                            defaultValue={0}
+                            onChange={(e) => { setQuantity(e.target.value) }}>
+                            {arrayRange(1, 20, 1).map((quantityOption) =>
+                                <option key={quantityOption}>{quantityOption}</option>
+                            )}
+                        </select>
+                    </nav>
+                    <nav id="shirt-size-nav">
+                        <label>Size: </label>
+                        <select
+                            id='shirt-size-nav-select'
+                            defaultValue="Size"
+                            onChange={(e) => { setSize(e.target.value) }}>
+                            {sizeList.map((sizeOption) =>
+                                sizeOption == 'Size:' ?
+                                    <option disabled key={sizeOption}>{sizeOption}</option> :
+                                    <option key={sizeOption}>{sizeOption}</option>
+                            )}
+                        </select>
+                    </nav>
+                    <button
+                        id="shirt-addToCartBtn"
+                        disabled={addToCartBtnDisabled}
+                        onClick={() => {
+                            console.log(side, color, size, quantity)
+                            console.log(`addToCartBtnDisabled: ${addToCartBtnDisabled}`)
+                        }}>
+                        Add to Cart
+                    </button>
                 </div>
             </div>
         </div>
     )
+}
+
+function arrayRange(start, stop, step) {
+    return Array.from(
+        { length: (stop - start) / step + 1 },
+        (value, index) => start + index * step
+    );
 }
